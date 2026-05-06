57dd0c92b4003f14e07d32818ce8fb593e100f64
Patrick LR7 <patrickrx7@gmail.com>
2026-05-05 23:25:18 -0600

    Change UserIcon on the Header.

--- components/Header.tsx
+++ components/Header.tsx
@@ -54,18 +54,17 @@ function UserMenu() {
   const UserIcon = (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      fill="none"
+      width="24"
+      height="24"
       viewBox="0 0 24 24"
-      strokeWidth={1.75}
+      fill="none"
       stroke="currentColor"
-      className="w-6 h-6"
-      aria-hidden="true"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
-      />
+      <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2" />
+      <circle cx="12" cy="7" r="4" />
     </svg>
   )
 
